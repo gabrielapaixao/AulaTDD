978797696738ee0b583ca1cbddc5094c5e6f148b
Gabriela Paixão <paixaogabriela00@gmail.com>
2020-10-02 16:53:37 -0300

    commit #2

--- Main.java
+++ Main.java
@@ -2,6 +2,8 @@ class Main {
   public static void main(String[] args) {
      Tests t = new Tests();
      t.testMultiplication();
+     t.testEquality();
+     t.testFrancMultiplication();
 	}
 
 }
